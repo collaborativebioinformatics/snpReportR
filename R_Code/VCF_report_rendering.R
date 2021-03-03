fcf7f91da613259281e6ff0504a8e2e38ee86138
Jenny Smith <jennyl.smith12@gmail.com>
2021-03-03 08:04:00 -0800

    fixed embedded images in email

--- R_Code/VCF_report_rendering.R
+++ R_Code/VCF_report_rendering.R
@@ -6,13 +6,10 @@ library(tidyr)
 library(rmarkdown)
 library(here)
 
-
-
-setwd(here())
+here::here()
 
 #Configure gmailr package. 
 #https://cran.r-project.org/web/packages/gmailr/vignettes/gmailr.html
-# Sys.getenv("GMAILR_APP")
 gmailr::gm_auth_configure() #this needs to be run at the top of the script to send emails
 # gm_auth()
 
@@ -25,27 +22,33 @@ sender         <- 'jennyl.smith.workonly@gmail.com'
 recipients     <- c('jennyl.smith.workonly@gmail.com')
 smtp.username  <- 'jennyl.smith.workonly@gmail.com'
 
+Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+
 #---------------------------------Define Parameters----------------------------------------------
 
 for (input_file in files){
   filename <- paste0(gsub("_applied.cancer.vcf|.wAnnot.vcf.gz","", basename(input_file)), "_snpReport.html")
   Params <- list(data=here(input_file))
-  Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
+
   rmarkdown::render(input = here("R_Code/Report_v2_JSmith.Rmd"),
                     output_file=filename, 
                     output_dir = here("Reports"),
                     params=Params)
   
-  outfile <- here(file.path("Reports", filename))
-  message <- paste(readLines(outfile, warn = FALSE), collapse = "\n")
+  email_message <- blastula::render_email('R_Code/Email_Body_JSmith.Rmd')
+  outfile <- here("Reports", filename) 
+
   
   email <- gmailr::gm_mime() %>%
     gmailr::gm_to(recipients) %>%
     gmailr::gm_from(sender) %>%
     gmailr::gm_subject("snpReportR") %>%
-    gmailr::gm_html_body(message) 
+    gmailr::gm_html_body(email_message$html_str) %>% 
+    gmailr::gm_attach_file(outfile) %>% 
+    gmailr::gm_attach_file(here::here("snpReporter_logo.png"), id = "foobar")
   
-  gmailr::gm_create_draft(email)
+  
+  # gmailr::gm_create_draft(email)
   gmailr::gm_send_message(mail=email)
 }
 
@@ -53,16 +56,3 @@ for (input_file in files){
 print("Finished Sending Email")
 
 
-### Testing 
-
-#https://cran.r-project.org/web/packages/gmailr/vignettes/sending_messages.html
-#This works!
-# email <- gm_mime() %>%
-#   gm_to(recipients) %>%
-#   gm_from(sender) %>%
-#   gm_subject("A Test Email") %>%
-#   gm_html_body(
-#     '<h1>A plot of <b>MotorTrend</b> data <i>(1974)</i></h1>
-#     <br><img src="cid:foobar">') 
-# gm_create_draft(email)
-# gm_send_message(mail=email)
